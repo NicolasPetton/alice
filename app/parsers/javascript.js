7cf99e9bebba1e16e0c714cf6736fab6d2de79fe
Angelo Delefortrie <angelo.delefortrie@gmail.com>
2019-01-11 21:15:26 +0100

    source configuration not needed

--- app/parsers/javascript.js
+++ app/parsers/javascript.js
@@ -3,7 +3,5 @@ const config = require('../config')
 
 module.exports = () => {
   const regexp = new RegExp(`(https?:)?(/{2})?${config.parsed_target.hostname}`, 'gi')
-  const replacer = [config.parsed_source.protocol, '//', config.parsed_source.hostname]
-
-  return replaceStream(regexp, replacer)
+  return replaceStream(regexp, '')
 }
